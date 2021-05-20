e34318335173212ac44f0e78f7080c0cafff98df
Niladri-Saha <77729328+Niladri-Saha@users.noreply.github.com>
2021-05-20 09:13:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var canvas=new fabric.Canvas('myCanvas');
+block_img_width=30;
+block_img_height=30;
+player_x=10;
+player_y=10;
+var player_object="";
+function player_update(){
+    fabric.Image.fromURL("player.png",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_object);    
+    });
+}
+function new_img(get_img){
+    fabric.Image.fromURL(get_img,function(Img){
+        block_object=Img;
+        block_object.scaleToWidth(block_img_width);
+        block_object.scaleToHeight(block_img_height);
+        block_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(block_object);    
+    });
+}
